d6bd5e34343faa189bcbf9de9437e5c93a6dd1b3
Franco Casas <casassfranco@gmail.com>
2021-06-08 16:20:35 -0300

    first coimmit cap 41

---
+++ WebAPIAutores/Controllers/AutoresController.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPIAutores.Entidades;
+
+namespace WebAPIAutores.Controllers
+{
+
+    //[HttpGet("/primer")] Para ignorar "Autores" en la url de la api
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AutoresController : ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+
+        public AutoresController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        //api/Autores
+        [HttpGet]
+        public async Task<ActionResult<Autor>> Get()
+        {
+            return await context.Autores.FirstOrDefaultAsync();
+        }
+
+        //api/Autores/primer
+        [HttpGet("primer")]
+        public async Task<ActionResult<Autor>> GetPrimerAutor()
+        {
+            var autor = await context.Autores.FirstOrDefaultAsync();
+
+            if(autor == null)
+            {
+                return NotFound();
+            }
+
+            return autor;
+        }
+
+        //api/Autores
+        [HttpPost]
+        public async Task<ActionResult> Post(Autor autor)
+        {
+            context.Add(autor);
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+
+        //api/Autores/:id
+        [HttpPut("{id:int}")]   // api/autores/:id
+        public async Task<ActionResult> Put(Autor autor, int id)
+        {
+            if(autor.Id != id)
+            {
+                return BadRequest("El id del autor no coincide con el id de la URL");
+            }
+
+            //Comprueba existencia
+            var existe = await context.Autores.AnyAsync(x => x.Id == id);
+
+            if (!existe)
+            {
+                return NotFound();
+            }
+
+            context.Update(autor);
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+
+        //api/Autores/:id
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult> Delete(int id)
+        { 
+            //Comprueba existencia
+            var existe = await context.Autores.AnyAsync(x => x.Id == id);
+
+            if (!existe)
+            {
+                return NotFound();
+            }
+
+            context.Remove(new Autor() { Id = id });
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+    }
+}
---
+++ WebAPIAutores/Entidades/Autor.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPIAutores.Helpers;
+
+namespace WebAPIAutores.Entidades
+{
+    public class Autor
+    {
+        public int Id { get; set; }
+        [Required]
+        //[PrimeraLetraMayuscula]
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+        //[CreditCard]
+        public string CreditCard { get; set; }
+        [Url]
+        public string Url { get; set; }
+
+        //Validar por entidad
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if(!string.IsNullOrEmpty(Nombre))
+            {
+                var primeraLetra = Nombre[0].ToString();
+
+                if (primeraLetra != primeraLetra.ToUpper()) 
+                {
+                    yield return new ValidationResult("La primera letra debe ser mayúscula", new string[] { nameof(Nombre) });
+                }
+            }
+        }
+    }
+}
